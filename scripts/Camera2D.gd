cd1f6f4bde2c656a58af3ab56c5ed1051fe7e720
orly <orly.yanson@myndconsulting.com>
2018-06-04 19:32:32 +0800

    [:sparkles:] added player with camera

--- scripts/Camera2D.gd
+++ scripts/Camera2D.gd
@@ -1,17 +1,22 @@
 extends Camera2D
 
+var main_char = preload("res://instances/Player.tscn")
 var cam_speed = 10
 
-func _ready():
-	set_process(true)
-	pass
+func _fixed_process(delta):
+	var target_pos = (get_node("Player").instance().get_global_pos()) * 0.5
+	set_global_pos(get_global_pos().linear_interpolate(target_pos, cam_speed * delta))
+
+#func _ready():
+#	set_process(true)
+#	pass
 	
-func _process(delta):
-	if Input.is_action_pressed('ui_up'):
-		translate(Vector2(0, -cam_speed))
-	if Input.is_action_pressed('ui_down'):
-		translate(Vector2(0, cam_speed))
-	if Input.is_action_pressed('ui_left'):
-		translate(Vector2(-cam_speed, 0))
-	if Input.is_action_pressed('ui_right'):
-		translate(Vector2(cam_speed, 0))
+#func _process(delta):
+#	if Input.is_action_pressed('ui_up'):
+#		translate(Vector2(0, -cam_speed))
+#	if Input.is_action_pressed('ui_down'):
+#		translate(Vector2(0, cam_speed))
+#	if Input.is_action_pressed('ui_left'):
+#		translate(Vector2(-cam_speed, 0))
+#	if Input.is_action_pressed('ui_right'):
+#		translate(Vector2(cam_speed, 0))
--- scripts/Chunk.gd
+++ scripts/Chunk.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var noise = preload("res://scripts/softnoise.gd")
 var block = preload("res://instances/Block.tscn")
+var main_char = preload("res://instances/Player.tscn")
 
 enum block_types {
 	GRASS, DIRT, STONE, BEDROCK
@@ -9,12 +10,14 @@ enum block_types {
 
 var world_depth = 10
 var surface_height = 60
+var walkable_surface = []
 var chunk_width = 60
 
 func _ready():
 	var softnoise = noise.SoftNoise.new(2342342341234)
 	for x in range(0, chunk_width):
 		var y = floor(softnoise.openSimplex2D((get_global_transform().origin.x / 128 + x) * .1, 0) * surface_height * .2)
+		walkable_surface.append(y+1)
 		new_block(Vector2(x*128,y*128), GRASS)
 		
 		#
@@ -26,6 +29,14 @@ func _ready():
 				new_block(Vector2(x*128,yy*128), STONE)
 			else:
 				new_block(Vector2(x*128,yy*128), DIRT)
+	
+	randomize()
+	var res_x = randi()%60
+	var res_y = randi()%60
+	print(res_x)
+	print(walkable_surface[res_x])
+	print(walkable_surface)
+	generate_main_char(Vector2(res_x*128,walkable_surface[res_x+1]*128))
 	pass
 	
 func new_block(pos, type):
@@ -33,3 +44,8 @@ func new_block(pos, type):
 	new_block.translate(pos)
 	new_block.block_type = type
 	add_child(new_block)
+	
+func generate_main_char(pos):
+	var character = main_char.instance()
+	character.translate(pos)
+	add_child(character)
